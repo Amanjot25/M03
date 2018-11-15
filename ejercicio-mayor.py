047217da7bef94aa178766a38968b841bea8e390
AmanjotSinghKaur <35691403+Amanjot25@users.noreply.github.com>
2018-11-15 11:40:15 +0100

    Create ejercicio-mayor.py

---
+++ ejercicio-mayor.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+#Con 2 numeros.
+num1 = int(input("Introduce un numero:"))
+num2 = int(input("Introduce otro numero:"))
+if (num1>num2):
+	print("El num1 es mayor", num1);
+else:
+    print("El num2 es mayor", num2);
+
+# coding: utf-8
+#Con 3 numeros.
+num1 = int(input("Introduce un numero:"))
+num2 = int(input("Introduce otro numero:"))
+num3 = int(input("Introduce otro numero:"))
+if (num1>num2) and (num1>num3):
+	print("El num1 es mayor", num1);
+else:
+    print("El num2 es mayor", num2);
+    print("El num3 es mayor", num3);
