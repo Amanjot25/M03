3a522b760cb1bf9a2a9e8ff56d854a852a7d8f95
AmanjotSinghKaur <35691403+Amanjot25@users.noreply.github.com>
2018-11-15 11:41:15 +0100

    Create ejercicio-swap-4.py

---
+++ ejercicio-swap-4.py
@@ -0,0 +1,13 @@
+#Coding:utf-8
+
+cajon_1="mobil"
+cajon_2="bocadillo"
+cajon_3="boli"
+cajon_4="bebida"
+cajon_ext=cajon_2
+cajon_2=cajon_4
+cajon_4=cajon_ext
+cajon_ext=cajon_1
+cajon_1=cajon_3
+cajon_3=cajon_ext
+print(cajon_1,cajon_2,cajon_3,cajon_4)
