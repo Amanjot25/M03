dc299ab86776f957b2f0da8b3ff1a11db1aac5c2
AmanjotSinghKaur <35691403+Amanjot25@users.noreply.github.com>
2018-11-15 11:38:53 +0100

    Create num_entero.py

---
+++ num_entero.py
@@ -0,0 +1,11 @@
+#Coding:utf-8 
+#joc de proves
+#Entrada        Sortida
+#  3            "positiu"
+# -2            "negatiu"
+#  0             "error"
+numero = int(input("Introduce un numero cualquiera")
+if (numero >= 0):
+    print("El numero es positivo",numero)
+else:
+    print("El numero es negativo",numero)
