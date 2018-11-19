8bb93f140b6c0bd05397c3a984d1dd00d10090c9
AmanjotSinghKaur <35691403+Amanjot25@users.noreply.github.com>
2018-11-19 12:46:18 +0100

    Update calculo-areas.py

--- calculo-areas.py
+++ calculo-areas.py
@@ -2,12 +2,12 @@
 print("Dime una figura geométrica:")
 area=input("¿Qué figura quiere calcular (Escriba T o C)?")
 if (area == "T"):
-    altura=int(input("Dime la altura:"))
-    base=int(input("Dime la base:"))
+    altura=float(input("Dime la altura:"))
+    base=float(input("Dime la base:"))
     respuesta=(base*altura/2)
     print("El triangulo tiene una area de",respuesta)
 else:
     if (area == "C"):
-        radio=int(input("Dime el radio:"))
+        radio=float(input("Dime el radio:"))
         respuesta=(3.14*radio**2)
     print("El circulo tiene una area de",respuesta)
