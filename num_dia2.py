983cf1add2cbbadb1bf06cfd6070632179c54702
AmanjotSinghKaur <35691403+Amanjot25@users.noreply.github.com>
2018-11-25 22:24:08 +0100

    Update num_dia2.py

--- num_dia2.py
+++ num_dia2.py
@@ -1,1 +1,7 @@
+#Coding:utf -8
+#Imprir los dias de 1 año 1 a 365 dias i marcar con "*" el dia de la luna llena (ciclos de la luna 28 dias,luna llena el primer dia del ciclo).
+for num_dia range (0,365):
+  print ("Volta nº",num_dia+1)
+if ((num_dia-1) % 28) == 0)):
+print ("*")
 
