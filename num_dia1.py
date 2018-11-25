fa5ff90af3f0c0b10b167f12bf02ff9913dfc7e8
AmanjotSinghKaur <35691403+Amanjot25@users.noreply.github.com>
2018-11-25 22:24:21 +0100

    Update num_dia1.py

--- num_dia1.py
+++ num_dia1.py
@@ -1,5 +1,5 @@
 #Coding:utf -8
-#Imprima 1 año a 365 dias i marcar con "*" el dia 70.
+#Imprir 1 año a 365 dias i marcar con "*" el dia 70.
 for num_dia range (0,365):
   print ("Volta nº",num_dia+1)
 if (num_dia % 365) == (70-1):
