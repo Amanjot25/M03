0b9096649c5b6f980386a005a1471bb693607451
AmanjotSinghKaur <35691403+Amanjot25@users.noreply.github.com>
2018-11-25 22:10:46 +0100

    Create num_dia3.py

---
+++ num_dia3.py
@@ -0,0 +1,9 @@
+#Coding:utf -8
+for num_dia range (0,365):
+  print ("Volta nº",num_dia+1)
+if (num_dia % 365) == (350-1):
+  print ("*")
+ 
+ 
+ #Cada 100 líneas que el código imprima intro cada vez que pulsemos una tecla cualquiera.
+ (Input=tecla)
