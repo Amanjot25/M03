fae9e5ce84fb5266e71c7c3b7953eba9011547aa
AmanjotSinghKaur <35691403+Amanjot25@users.noreply.github.com>
2018-12-02 22:53:53 +0100

    Create ejercicio-condicion-chunga.py

---
+++ ejercicio-condicion-chunga.py
@@ -0,0 +1,15 @@
+#Coding: utf-8
+
+numero=int(input("Dime un numero: "))
+if(numero%2==0):
+    print("Es un numero par")
+else:
+    print("Es un numero impar")
+if(numero<=40)and(numero>=-10):
+    print("Esta entre 40 y -10")
+else:
+    print("No esta entre 40 y -10")
+if(numero<0):
+    print("Es negativo.")
+else:
+print("No es negativo.")
